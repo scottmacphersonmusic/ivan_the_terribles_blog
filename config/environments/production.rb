c0883b4ef1c3307bd1c4e9973ccea8c390f36ab1
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-10-01 00:18:20 -0700

    Add Kaminari to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,8 @@
 IvanTheTerriblesBlog::Application.configure do
+  Kaminari.configure do |config|
+    config.default_per_page = 20
+  end
+
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
